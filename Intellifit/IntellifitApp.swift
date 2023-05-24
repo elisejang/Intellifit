8ecf7de56a1ab9deed78458ea353adbba944626b
Elise Jang <elisej1@uci.edu>
2023-05-23 19:30:53 -0700

    Initial Commit

---
+++ Intellifit/IntellifitApp.swift
@@ -0,0 +1,17 @@
+//
+//  IntellifitApp.swift
+//  Intellifit
+//
+//  Created by Elise Jang on 5/23/23.
+//
+
+import SwiftUI
+
+@main
+struct IntellifitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
