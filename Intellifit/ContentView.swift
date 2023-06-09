e57a41cb7986a4da545ebca15e3e08c1a9dfaa84
Megan Nguyen <53189086+megannguyen02@users.noreply.github.com>
2023-06-09 00:45:06 -0700

    Update ContentView.swift

--- Intellifit/ContentView.swift
+++ Intellifit/ContentView.swift
@@ -8,40 +8,48 @@
 import SwiftUI
 
 struct ContentView: View {
-    // test
+    @State var isNewUser = true
+    @State private var isShowingNewUserPopUp = false
+    @State private var isShowingHomeView = false
+
     var body: some View {
-        VStack {
-            Image("Logo")
-                .resizable()
-                .scaledToFit()
-                .frame( height: 50)
-            Text("Intellifit!")
-            TabView {
-                HomeView()
-                    .tabItem() {
-                        Image(systemName: "house.fill")
-                        Text("Home")
+        if isNewUser{
+            NewUserView(isNewUser: $isNewUser, isShowingHomeView: $isShowingHomeView)
+        } else if isShowingHomeView {
+            ZStack{
+                VStack {
+                    Image("Logo")
+                        .resizable()
+                        .scaledToFit()
+                        .frame( height: 40)
+                    TabView {
+                        HomeView()
+                            .tabItem() {
+                                Image(systemName: "house.fill")
+                                Text("Home")
+                            }
+                        WorkoutView()
+                            .tabItem() {
+                                Image(systemName: "dumbbell.fill")
+                                Text("Workout")
+                            }
+                        FoodView()
+                            .tabItem() {
+                                Image(systemName: "fork.knife")
+                                Text("Food")
+                            }
+                        SleepView()
+                            .tabItem() {
+                                Image(systemName: "moon.fill")
+                                Text("Sleep")
+                            }
+                        ProfileView()
+                            .tabItem() {
+                                Image(systemName: "person.crop.circle")
+                                Text("Profile")
+                            }
                     }
-                WorkoutView()
-                    .tabItem() {
-                        Image(systemName: "dumbbell.fill")
-                        Text("Workout")
-                    }
-                FoodView()
-                    .tabItem() {
-                        Image(systemName: "fork.knife")
-                        Text("Food")
-                    }
-                SleepView()
-                    .tabItem() {
-                        Image(systemName: "moon.fill")
-                        Text("Sleep")
-                    }
-                ProfileView()
-                    .tabItem() {
-                        Image(systemName: "person.crop.circle")
-                        Text("Profile")
-                    }
+                }
             }
         }
     }
